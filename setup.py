c2dc6d06a6a8ff8615c251a9e8dcadabfc1030af
SK <skob@me.com>
2019-03-28 15:54:35 +0300

    manual prefix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-fm-minio',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',  # example license
